b47ef7778782a22fe5108f91fd4161cba84987ef
lichye <yeleiqi@hotmail.com>
2025-01-11 15:15:19 +0000

    add install shell

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
-mkdir otherTools
-wget https://github.com/YosysHQ/oss-cad-suite-build/releases/download/2025-01-01/oss-cad-suite-linux-x64-20250101.tgz
-tar zxvf oss-cad-suite-linux-x64-20250101.tgz
+# Install cvc5
+git clone https://github.com/cvc5/cvc5.git
+#This is cvc5 version 1.0.1-dev.2.77d0bec48 [git 77d0bec48 on branch main with local modifications]
+
+# Install cocotb
+pip install "cocotb==1.8.0"
+
+# Install Pip-test
+pip install pytest
+
+#install yosys
+wget -nc https://github.com/YosysHQ/oss-cad-suite-build/releases/download/2023-05-20/oss-cad-suite-linux-x64-20230520.tgz
+tar -xvzf oss-cad-suite-linux-x64-20230520.tgz
