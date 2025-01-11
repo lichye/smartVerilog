9d9b3831bb7932e018124f81eb477e52a266cdbb
lichye <yeleiqi@hotmail.com>
2025-01-11 00:58:34 -0800

    start to work on install shell

---
+++ install.sh
@@ -0,0 +1,3 @@
+mkdir otherTools
+wget https://github.com/YosysHQ/oss-cad-suite-build/releases/download/2025-01-01/oss-cad-suite-linux-x64-20250101.tgz
+tar zxvf oss-cad-suite-linux-x64-20250101.tgz
