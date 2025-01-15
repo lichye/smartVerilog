ac12989ec9474f32543ae000375c97a34ad1ca7a
lichye <yeleiqi@hotmail.com>
2025-01-15 13:34:48 +0000

    update timeout

--- smart/evaluater.py
+++ smart/evaluater.py
@@ -130,7 +130,7 @@ def run_fm_on_verilog_files(directory, properties, sby_path="sby"):
                     sby_run_file.write("bmc_check:\n")
                     sby_run_file.write("mode bmc\n")
                     sby_run_file.write("depth 10\n")
-                    sby_run_file.write("timeout 10000\n")
+                    sby_run_file.write("timeout 100\n")
                     sby_run_file.write("vcd_sim on\n")
                     sby_run_file.write("\n")
                     sby_run_file.write("[engines]\n")
@@ -146,11 +146,11 @@ def run_fm_on_verilog_files(directory, properties, sby_path="sby"):
                     sby_run_file.write("prep -top "+top_module+"\n")
                     
 
-                cmd = ["timeout","20",sby_path,"-f",sby_file,"task"]
+                cmd = ["timeout","30",sby_path,"-f",sby_file,"task"]
                 # print("cmd: ",cmd)
                 # cmd = [sby_path,"-f",sby_file,"task"]
                 
-                cmd2 = ["timeout","20","ebmc",new_file_path,"--bound","10","--top",top_module]
+                cmd2 = ["timeout","30","ebmc",new_file_path,"--bound","10","--top",top_module]
 
                 # cmd2 = ["ebmc",new_file_path,"--bound","10","--top",top_module]
 
