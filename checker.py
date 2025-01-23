7cdafa304e9c77db28d0f895d91b2ff2cd23bcfb
lichye <yeleiqi@hotmail.com>
2025-01-23 16:57:59 +0000

    update

--- checker.py
+++ checker.py
@@ -56,7 +56,7 @@ def write_assertion_file(input_file, output_file, assertions):
 def run_fm_on_verilog_file(verilog_file,properties,verilog_related_files):
     time_start = time.time()
     unique_id = uuid.uuid4() 
-    sby_file = os.path.join(os.getcwd(), f"dis_{unique_id}.sby")
+    sby_file = os.path.join(os.getcwd(), f"dis.sby")
     sby_result = os.path.join(os.getcwd(), f"dis_{unique_id}_task")
 
     try:
@@ -96,36 +96,6 @@ def run_fm_on_verilog_file(verilog_file,properties,verilog_related_files):
                 # file.write("prep -top "+)
                 sby_run_file.write("prep -top "+top_module+"\n")
                 
-
-            cmd = ["timeout","180","sby","-f",sby_file,"task"]
-            # print("cmd: ",cmd)
-            # cmd = [sby_path,"-f",sby_file,"task"]
-            
-            cmd2 = ["timeout","180","ebmc",new_file_path,"--bound","10","--top",top_module]
-
-            # cmd2 = ["ebmc",new_file_path,"--bound","10","--top",top_module]
-
-            result = subprocess.run(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
-            
-            if top_module in ebmc_unsupport_list:
-                if(result.returncode != 0):
-                    if(result.returncode ==124):
-                        return_result.append({verilog_file:"timeout"})
-                    else:
-                        return_result.append({verilog_file:"error"})
-                else:
-                    return_result.append({verilog_file:"verified"})
-            else:
-                result2 = subprocess.run(cmd2,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
-                # print("the return code is: ",result.returncode)                     
-                if(result.returncode != 0 or result2.returncode != 0):
-                    if(result.returncode ==124 or result2.returncode == 124):
-                        return_result.append({verilog_file:"timeout"})
-                    else:
-                        return_result.append({verilog_file:"error"})
-                else:
-                    return_result.append({verilog_file:"verified"})
-
         except Exception as e:
             print(f"Failed to create .sby file: {e}")
             return_result.append({verilog_file:"error"})
@@ -133,131 +103,17 @@ def run_fm_on_verilog_file(verilog_file,properties,verilog_related_files):
     except Exception as e:
         print(f"Failed to run sby on {verilog_file}: {e}")
         return_result.append({verilog_file:"error"})
-    subprocess.run(["rm","-rf",sby_file])
-    subprocess.run(["rm","-rf",sby_result])
     time_end = time.time()
     print("Finish evaluate time: "+str(time_end-time_start)+" on the file: "+verilog_file + "with sby file: "+sby_file)
     return return_result
 
-def run_fm_on_verilog_files(directory, properties, sby_path="sby"):
-    """
-    Automatically runs `sby` on all Verilog files named mutant_*.sv in the specified directory.
-
-    Parameters:
-        directory (str): Path to the directory containing Verilog files.
-        sby_path (str): Path to the `sby` executable (default: "sby").
-
-    Returns:
-        None
-    """
-    print("The directory is: ",directory)
-    # Check if the directory exists
-    if not os.path.exists(directory):
-        print(f"Error: Directory '{directory}' does not exist.")
-        return
-
-    # Get all Verilog files matching mutant_*.sv in the directory
-    verilog_files = [
-        os.path.join(directory, file)
-        for file in os.listdir(directory)
-        if file.startswith("mutant_") and file.endswith(".sv") and not file.endswith("_assertion.sv")
-    ]
-
-    verilog_related_files =[
-        os.path.join(directory, file)
-        for file in os.listdir(directory)
-        if not file.startswith("mutant_") and file.endswith(".sv") and not file.endswith("_assertion.sv")
-    ]
-
-    if not verilog_files:
-        print("No matching Verilog files found in the specified directory.")
-        return
-
-    print(f"Found {len(verilog_files)} Verilog files. Starting to process...")
-
-    error_files = []  # List to track files with errors
-    thread_result = []
-    with ProcessPoolExecutor(max_workers=10) as executor:
-        futures = [
-            executor.submit(run_fm_on_verilog_file, verilog_file, properties, verilog_related_files)
-            for verilog_file in verilog_files
-        ]
-        for future in futures:
-            thread_result += future.result()
-        print("Finish parallel processing")
-    # print("The result is: ",thread_result)
-
-    for result in thread_result:
-        filename = result.keys()
-        value = result.values()
-        if list(value)[0] == "error":
-            error_files.append(list(filename)[0])
-        if list(value)[0] == "timeout":
-            timeout_list.append(list(filename)[0])
-
-    return error_files
-
-def get_result_files(directory):
-
-    result_files = set()
-    
-    if not os.path.exists(directory):
-        raise FileNotFoundError(f"Directory '{directory}' does not exist.")
-
-    for filename in os.listdir(directory):
-        full_path = os.path.join(directory, filename)
-        if filename.startswith("result") and os.path.isfile(full_path):
-            result_files.add(full_path)
-
-    return result_files
-
-def get_mutant_files(directory):
-    
-    mutant_files = set()
-        
-    if not os.path.exists(directory):
-        raise FileNotFoundError(f"Directory '{directory}' does not exist.")
-    
-    for filename in os.listdir(directory):
-        full_path = os.path.join(directory, filename)
-        if filename.startswith("mutant") and os.path.isfile(full_path):
-            mutant_files.add(full_path)
-    return mutant_files
 
 def read_file(file):
     with open(file, "r") as file:
-        content = file.read()
-    return content
-
-def count_logfile():
-
-    with open('log.txt', 'r') as file:
-        logfile_content = file.read()
+        content = file.readlines()
     
-    fm_checker_pattern = re.compile(r"FM checker are called (\d+) times")
-    fm_timer_pattern = re.compile(r"FM checker Timer: ([\d\.]+) seconds")
-    cvc5_pattern = re.compile(r"CVC5 are called (\d+) times")
-    cvc5_timer_pattern = re.compile(r"CVC5 Timer: ([\d\.]+) seconds")
-
-    fm_checker_calls = 0
-    fm_checker_time = 0.0
-    cvc5_calls = 0
-    cvc5_time = 0.0
-
-    for match in fm_checker_pattern.finditer(logfile_content):
-        fm_checker_calls += int(match.group(1))
-    for match in fm_timer_pattern.finditer(logfile_content):
-        fm_checker_time += float(match.group(1))
-    for match in cvc5_pattern.finditer(logfile_content):
-        cvc5_calls += int(match.group(1))
-    for match in cvc5_timer_pattern.finditer(logfile_content):
-        cvc5_time += float(match.group(1))
-
-    result = "FM Checker Total Calls: " + str(fm_checker_calls) + "\n"
-    result += "FM Checker Total Time: " + str(fm_checker_time) + " seconds\n"
-    result += "CVC5 Total Calls: " + str(cvc5_calls) + "\n"
-    result += "CVC5 Total Time: " + str(cvc5_time) + " seconds\n"
-    return result
+    content = [content.strip() for content in content]
+    return content
 
 if __name__ == "__main__":
     if(len(sys.argv) < 2):
@@ -266,7 +122,14 @@ if __name__ == "__main__":
     else:
         top_module = sys.argv[1]
 
-    property_file = "result.txt"
+    
    
     working_dir = os.getcwd()
-    results = read_file(property_file)
+
+    property_file = working_dir+"/result.txt"
+    
+    properties = read_file(property_file)
+
+    runfile = working_dir+"/c432.sv"
+
+    run_fm_on_verilog_file(runfile,properties,[])
