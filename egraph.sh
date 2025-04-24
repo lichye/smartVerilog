5f526b04a0ebdd39e54e7aa2921594d9141a92b8
lichye <yeleiqi@hotmail.com>
2025-04-24 14:19:45 +0100

    add egraph

---
+++ egraph.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Check $1 is the name of the file
+if [ $# -lt 1 ]; then
+    echo "Lack of arguments."
+    echo "Usage: $0 <benchmark_name>"
+    exit 1
+fi
+src_benchmarks="Benchmark/$1"
+
+if [ -d "$src_benchmarks" ]; then
+    echo "Directory $src_benchmarks exists."
+else
+    echo "Directory $src_benchmarks does not exist."
+    exit 1
+fi
+
+source otherTools/venv/bin/activate
+source otherTools/oss-cad-suite/environment
+
+cd smart
+make all_clean
+cd ..
+
+DEST_DIR="smart/user"
+echo "Full path: $src_benchmarks"
+cp "$src_benchmarks"/* "$DEST_DIR"
+
+cd smart
+mkdir user/
+python setup.py $1
+rm -rf *task
+python smart.py $1
+assert_egraph -f SygusResult.sl -o egraph.txt
+python src/python/countline.py egraph.txt
---
+++ smart/src/python/countline.py
@@ -0,0 +1,18 @@
+import sys
+def countline(input_path, output_path):
+    count = 0
+    with open(input_path, 'r') as infile, open(output_path, 'w') as outfile:
+        for line in infile:
+            count= count + 1
+    return count
+
+
+if __name__ == "__main__":
+    if(len(sys.argv) != 2):
+        print("Usage: python Sygus2Verilog.py <input_file>")
+        sys.exit(1)
+    else:
+        input_file = sys.argv[1]
+    # Example usage
+    number = countline(input_file)
+    print("Number of lines in the file:", number)
